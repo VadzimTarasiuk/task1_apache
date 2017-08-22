1f1ac3fb45239a7f0c0ab7a6daf9ac58f747d184
Vadzim Tarasiuk <vad.tarasyuk@gmail.com>
2017-08-22 09:13:45 +0300

    Moved on

--- task1_apache/cookbooks/task1_apache/recipes/default.rb
+++ task1_apache/cookbooks/task1_apache/recipes/default.rb
@@ -3,3 +3,22 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+package 'httpd' do
+  action :install
+end
+
+package 'php' do
+  action :install
+end
+
+file '/var/www/html/test.php' do
+  content "<?php\n phpinfo();\n ?>"
+  mode '0755'
+  owner 'vagrant'
+  group 'vagrant'
+end
+
+service 'httpd' do
+  action [ :enable, :start ]
+end
